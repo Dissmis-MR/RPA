767fcc90decb5377feea5aa451497cd6eab5ec4b
dismis <rolih.marko@gmail.com>
2018-04-23 09:52:35 +0200

    vaja1-samostojno

---
+++ Vaja1-sam/Vaja1-sam/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Vaja1_sam
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
